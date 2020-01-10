1ba253366727dca98065ded7a40844254bcd9507
John Melody Me <johnmelodyme@icloud.com>
2020-01-10 21:17:22 +0800

    Button Deauther added

---
+++ app/src/main/java/com/johnmelodyme/hackertool/DEAUTHER.java
@@ -0,0 +1,14 @@
+package com.johnmelodyme.hackertool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DEAUTHER extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deauther);
+    }
+}
--- app/src/main/java/com/johnmelodyme/hackertool/MainActivity.java
+++ app/src/main/java/com/johnmelodyme/hackertool/MainActivity.java
@@ -2,13 +2,36 @@ package com.johnmelodyme.hackertool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+/**
+ * @AUTHOR : JOHN MELODY ME
+ * @COPYRIGHT : JOHN MELODY ME
+ * @PROJECT: HACKER TOOL
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private Button DEAUTHER;
+
+    private void INIT(){
+        DEAUTHER = (Button) findViewById(R.id.wifi_deauther);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        INIT();
+
+        DEAUTHER.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent DEAUTHER;
+                DEAUTHER =  new Intent(MainActivity.this, DEAUTHER.class);
+
+            }
+        });
     }
 }
