bcc5eef000a79f8531b90052ed36577a13a10864
John Melody Me and Tan sin dee <johnmelodyme@icloud.com>
2020-02-27 12:23:57 +0800

    Deauther Function Updated

--- app/src/main/java/com/johnmelodyme/hackertool/MAINMENU.java
+++ app/src/main/java/com/johnmelodyme/hackertool/MAINMENU.java
@@ -1,100 +1,30 @@
 package com.johnmelodyme.hackertool;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
-import java.util.ArrayList;
-// https://github.com/johnmelodyme/sweet-alert-dialog
-import cn.pedant.SweetAlert.SweetAlertDialog;
-
-/**
- * @AUTHOR : JOHN MELODY ME
- * @COPYRIGHT : JOHN MELODY ME
- * @PROJECT: HACKER TOOL
- */
 
 public class MAINMENU extends AppCompatActivity {
+    private static final String TAG = "hackertool";
+    private Button Termux;
 
-    private Dialog DIALOGUE;
-    private int WIFI_REQUEST_CODE = 0b0;
-    private static final String TAG = MAINMENU.class.getName();
-    private Button DEAUTHER, TERMINAL;
-    private ListView WIFILIST;
-    private AlertDialog.Builder alertdialogbuilder;
-    ArrayList list;
-    private ArrayAdapter Adapter;
-
-
-    private void INIT(){
-        DEAUTHER = (Button) findViewById(R.id.wifi_deauther);
-        WIFILIST = (ListView) findViewById(R.id.wifilist);
-        TERMINAL = (Button) findViewById(R.id.TERMUX);
-
+    private void declaractionInit() {
+        Termux = findViewById(R.id.termux);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        INIT();
+        declaractionInit();
 
-        // https://bit.ly/36L6ES6
-        DEAUTHER.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new SweetAlertDialog(MAINMENU.this, SweetAlertDialog.WARNING_TYPE)
-                        .setTitleText("ARE YOU CONNECTED TO WIFI NAMED \"pwned\"?")
-                        .setConfirmText("YES, I AM.")
-                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                            @Override
-                            public void onClick(SweetAlertDialog sDialog) {
-                                sDialog.dismissWithAnimation();
-                                Intent DEAUTHER;
-                                DEAUTHER = new Intent(Intent.ACTION_VIEW,
-                                        Uri.parse("http://192.168.4.1/"));
-                                startActivity(DEAUTHER);
-                            }
-                        })
-                        .setCancelButton("NO", new SweetAlertDialog.OnSweetClickListener() {
-                            @Override
-                            public void onClick(SweetAlertDialog sDialog) {
-                                sDialog.dismissWithAnimation();
-                                Intent WIFI;
-                                WIFI = new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS);
-                                startActivityForResult(WIFI, WIFI_REQUEST_CODE);
-                            }
-                        })
-                        .show();
-            }
-        });
-
-        DEAUTHER.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                String MSG;
-                MSG = getResources().getString(R.string.wifideauther);
-                new SweetAlertDialog(MAINMENU.this, SweetAlertDialog.BUTTON_POSITIVE)
-                        .setTitleText(MSG)
-                        .setConfirmText("OKAY")
-                        .show();
-                return false;
-            }
-        });
-
-        TERMINAL.setOnClickListener(new View.OnClickListener() {
+        Termux.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean isTermuxInstalled;
@@ -102,6 +32,7 @@ public class MAINMENU extends AppCompatActivity {
                 if (isTermuxInstalled){
                     Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.termux");
                     if (launchIntent != null) {
+                        Log.d(TAG, "Opening Termux");
                         startActivity(launchIntent);
                     } else {
                         showtoast("PLEASE INSTALL TERMUX");
@@ -112,7 +43,6 @@ public class MAINMENU extends AppCompatActivity {
                             Uri.parse("https://play.google.com/store/apps/details?id=com.termux&hl=en"));
                     startActivity(TERMUX);
                 }
-
             }
         });
     }
@@ -123,7 +53,7 @@ public class MAINMENU extends AppCompatActivity {
             pm.getPackageInfo(s, PackageManager.GET_ACTIVITIES);
             return true;
         } catch (PackageManager.NameNotFoundException e) {
-            Log.e(TAG, "TERMUX NOT INSTALLED.");
+            Log.d(TAG, "TERMUX NOT INSTALLED.");
         }
         return false;
     }
